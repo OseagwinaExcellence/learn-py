69a5ee08ad8ebafa271a6182a5dd2d543ac33a80
Oseagwina Excellence <oseagwinaexcellence@gmail.com>
2025-06-25 17:38:25 +0100

    up

--- test.py
+++ test.py
@@ -5,5 +5,6 @@ message = ("is too short")
 message_2 = (" is Good✅ ")
 if password < password_length:
      print(f"{password} {message}")
+     print(f"password must be 8 characters long2")
 else:
-          print(f"{password} {message_2}")j
+     print(f"{password} {message_2}")
