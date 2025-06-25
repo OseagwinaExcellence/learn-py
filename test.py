07f955217a3bfa04ce524259b5a5b5f835d30b31
Oseagwina Excellence <oseagwinaexcellence@gmail.com>
2025-06-25 15:10:32 +0100

    UPDATE

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 password = input("Enter Password: ")
 password_length = str(8)
+
 message = ("is too short")
 message_2 = (" is Good✅ ")
 if password < password_length:
      print(f"{password} {message}")
-     if password >= password_length:
-          print(f"{password} {message_2}")
-
 else:
-     print(f"{password} {message_2}")
+          print(f"{password} {message_2}")
